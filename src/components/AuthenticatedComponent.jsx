393b26690b53fbb9a0a325f94c8a04c1365a4e43
Martin Gontovnikas <martin@gon.to>
2015-04-06 15:37:28 -0300

    Added enters

--- src/components/AuthenticatedComponent.jsx
+++ src/components/AuthenticatedComponent.jsx
@@ -4,41 +4,42 @@ import LoginStore from '../stores/LoginStore';
 export default (ComposedComponent) => {
   return class AuthenticatedComponent extends React.Component {
 
-  static willTransitionTo(transition) {
-    if (!LoginStore.isLoggedIn()) {
-      transition.redirect('/login', {}, {'nextPath' : transition.path});
+    static willTransitionTo(transition) {
+      if (!LoginStore.isLoggedIn()) {
+        transition.redirect('/login', {}, {'nextPath' : transition.path});
+      }
     }
-  }
 
-  constructor() {
-    this.state = this._getLoginState();
-  }
+    constructor() {
+      this.state = this._getLoginState();
+    }
 
-  _getLoginState() {
-    return {
-      userLoggedIn: LoginStore.isLoggedIn(),
-      user: LoginStore.user
-    };
-  }
+    _getLoginState() {
+      return {
+        userLoggedIn: LoginStore.isLoggedIn(),
+        user: LoginStore.user
+      };
+    }
 
-  componentDidMount() {
-    LoginStore.addChangeListener(this._onChange.bind(this));
-  }
+    componentDidMount() {
+      LoginStore.addChangeListener(this._onChange.bind(this));
+    }
 
-  _onChange() {
-    this.setState(this._getLoginState());
-  }
+    _onChange() {
+      this.setState(this._getLoginState());
+    }
 
-  componentWillUnmount() {
-    LoginStore.removeChangeListener(this._onChange.bind(this));
-  }
+    componentWillUnmount() {
+      LoginStore.removeChangeListener(this._onChange.bind(this));
+    }
 
-  render() {
-    return (<ComposedComponent {...this.props}
-      user={this.state.user} userLoggedIn={this.state.userLoggedIn} />
-    );
-  }
-
-
+    render() {
+      return (
+      <ComposedComponent
+        {...this.props}
+        user={this.state.user}
+        userLoggedIn={this.state.userLoggedIn} />
+      );
+    }
   }
 };
