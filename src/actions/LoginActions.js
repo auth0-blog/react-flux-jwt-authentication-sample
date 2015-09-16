644556c81331e9ec80bdbeaf3d6ba6d22f3d3faa
Manish Sinha <arithmetic@gmail.com>
2015-09-16 02:00:01 -0400

    Update LoginActions.js

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -5,18 +5,18 @@ import RouterContainer from '../services/RouterContainer'
 export default {
   loginUser: (jwt) => {
     var savedJwt = localStorage.getItem('jwt');
-
+    
+    AppDispatcher.dispatch({
+      actionType: LOGIN_USER,
+      jwt: jwt
+    });
+    
     if (savedJwt !== jwt) {
       var nextPath = RouterContainer.get().getCurrentQuery().nextPath || '/';
 
       RouterContainer.get().transitionTo(nextPath);
       localStorage.setItem('jwt', jwt);
     }
-
-    AppDispatcher.dispatch({
-      actionType: LOGIN_USER,
-      jwt: jwt
-    });
   },
   logoutUser: () => {
     RouterContainer.get().transitionTo('/login');
